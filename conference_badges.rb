35968ce7020c1b92429ca7467bdc03b4a0991c2c
Charlie Barth <charliepbarth@gmail.com>
2020-06-23 21:02:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,37 @@
-# Write your code here.
+
+def badge_maker(name)
+  
+  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator(array)
+  array_new = []
+  
+  array.each do |name|
+    array_new.push("Hello, my name is #{name}.")
+  end
+  return array_new
+end
+
+
+def assign_rooms(array)
+  array_two = []
+  counter = 0
+  
+  array.each do |name|
+    counter += 1
+    array_two.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  end
+  return array_two
+end
+
+
+def printer(attendees)
+  array_ruler = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  puts batch_badge_creator(array_ruler)
+  puts assign_rooms(array_ruler)
+  
+end
+printer("Why")
